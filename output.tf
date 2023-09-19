ef630d5b4bd101c05ef9b542d9463c6426e2660d
angelaiondem <angelaiondem@yahoo.com>
2023-09-19 12:19:23 +0200

    Revise the output data.

--- output.tf
+++ output.tf
@@ -1,19 +1,49 @@
-output "vpc_id" {
-    description = "VPC ID"
-    value = aws_vpc.main_vpc.id
-}
-
-output "vpc_cidr_block" {
-    description = "VPC CIDR Block"
-    value = aws_vpc.main_vpc.cidr_block
-}
-
 output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
+  description = "Public IP address of the EC2 instance with Jenkins Master"
   value       = aws_instance.jenkins_instance.public_ip
 }
 
 output "instance_private_ip" {
-  description = "Public IP address of the EC2 instance"
+  description = "Public IP address of the EC2 instance with Jenkins Master"
   value       = aws_instance.jenkins_instance.private_ip
+}
+
+output "vpc_id" {
+    description = "Main VPC ID of the Project"
+    value = aws_vpc.main_vpc.id
+}
+
+output "public_subnet_1_id" {
+    description = "Public Subnet 1 ID"
+    value = aws_subnet.public_subnets[0].id
+}
+
+output "public_subnet_2_id" {
+    description = "Public Subnet 2 ID"
+    value = aws_subnet.public_subnets[1].id
+}
+
+output "public_subnet_3_id" {
+    description = "Public Subnet 3 ID"
+    value = aws_subnet.public_subnets[2].id
+}
+
+output "private_subnet_1_id" {
+    description = "Public Subnet 1 ID"
+    value = aws_subnet.private_subnets[0].id
+}
+
+output "private_subnet_2_id" {
+    description = "Public Subnet 1 ID"
+    value = aws_subnet.private_subnets[1].id
+}
+
+output "database_subnet_1_id" {
+    description = "Public Subnet 1 ID"
+    value = aws_subnet.database_subnets[0].id
+}
+
+output "database_subnet_2_id" {
+    description = "Public Subnet 1 ID"
+    value = aws_subnet.database_subnets[1].id
 }
